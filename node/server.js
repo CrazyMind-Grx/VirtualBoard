0437753ec87fff56fcfb201bc28586d6021fdce7
Angel JCC <angeljcc@gmail.com>
2014-11-07 17:19:13 +0100

    borro carpeta node

--- node/server.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('servidor creado\n');
-}).listen(8080);
-
-console.log('Server listen to the port 8080');
