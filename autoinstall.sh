dcdcbe329a3aed6e681bbba927a6e9f7b0358b72
javidebaza <j.arandaizquierdo@gmail.com>
2014-11-07 13:14:05 +0100

    update autoinstall

--- autoinstall.sh
+++ autoinstall.sh
@@ -1,6 +1,15 @@
 cd ~;
 echo "\n \n \n ........................................................................\n Descargando e instalando el entorno de desarrollo para Android \n ........................................................................ \n \n \n";
 
+echo "¿Quiere descargar Eclipse + SDK de Android en el directorio actual? (si elige no, se descargara en su directorio /home)   (s o n): ";
+read var1;
+
+if [ $var1 = s ]; then
+	pwd;
+ 
+
+read var1
+
 identificarSO=`uname -i`;
 echo "La arquitectura de tu sistema operativo es de: "$identificarSO " \n \n \n \n";
 
