310fe0d0efd03b209421047175c80be2fa1a2960
angeljcc <angeljcc@gmail.com>
2014-12-11 20:50:04 +0100

    intento de conexion de app al servidor
    no hay manera :-1:

--- AppCode/VB/app/src/main/java/com/vb/angel/vb/MainActivity.java
+++ AppCode/VB/app/src/main/java/com/vb/angel/vb/MainActivity.java
@@ -11,8 +11,21 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 
 public class MainActivity extends ActionBarActivity {
+    private String ip;
+    private int puerto;
+    Socket miCliente;
+    ObjectOutputStream oos;
+    ObjectInputStream ois;
+    String mdata;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +36,10 @@ public class MainActivity extends ActionBarActivity {
         final EditText mensaje = (EditText)findViewById(R.id.escribir);
         final TextView scroll = (TextView)findViewById(R.id.scroll);
         final String vacio="";
+
+        ip = "vs-angeljcc.c9.io";
+        puerto = 8080;
+
         boton_enviar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -31,9 +48,11 @@ public class MainActivity extends ActionBarActivity {
                     Toast.makeText(mensaje.getContext(), "El campo esta vacío, no se enviará nada!!", Toast.LENGTH_LONG).show();
                 }else{
                     Toast.makeText(mensaje.getContext(), "escribiste: "+mensaje.getText(), Toast.LENGTH_LONG).show();
-                    scroll.setText(mensaje.getText().toString());
+                    //scroll.setText(mensaje.getText().toString());
+                    Connect();
+                    Snd_Msg(mensaje.getText().toString());
+                    Disconnect();
                     mensaje.setText("");
-
                 }
             }
         });
@@ -48,6 +67,68 @@ public class MainActivity extends ActionBarActivity {
                 return false;
             }
         });
+    }
+
+    //Funciones de  conexion y desconexion
+    public boolean Connect() {
+        //Obtengo datos ingresados en campos
+
+        try {//creamos sockets con los valores anteriores
+            miCliente = new Socket("https://vs-angeljcc.c9.io",8080);
+            //si nos conectamos
+            if (miCliente.isConnected()) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (UnknownHostException e) {
+            //Si hubo algun error mostrmos error
+
+        }catch (IOException e){
+
+        }
+        return false;
+    }
+
+    public void Disconnect() {
+        try {
+            miCliente.close();
+
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "Desconectado...", Toast.LENGTH_LONG).show();
+        }
+
+
+
+    }
+    //FUncion de enviar mensaje
+    public boolean Snd_Msg(String s) {
+
+        try {
+            //Accedo a flujo de salida
+            oos = new ObjectOutputStream(miCliente.getOutputStream());
+            //creo objeto mensaje
+
+
+            if (miCliente.isConnected())// si la conexion continua
+            {
+                //lo asocio al mensaje recibido
+                ;
+                //Envio mensaje por flujo
+                oos.writeObject(s);
+                //envio ok
+                return true;
+
+            } else {//en caso de que no halla conexion al enviar el msg
+                Toast.makeText(getApplicationContext(), "No hay conexion", Toast.LENGTH_LONG).show();
+                return false;
+            }
+
+        } catch (Exception e) {// hubo algun error
+            Toast.makeText(getApplicationContext(), "Hubo algun error DEBUG!!!", Toast.LENGTH_LONG).show();
+
+            return false;
+        }
     }
 
 
