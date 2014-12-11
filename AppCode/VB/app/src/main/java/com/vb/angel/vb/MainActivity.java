a637607a7227de3e064c8c37b6b027c96f08d701
angeljcc <angeljcc@gmail.com>
2014-12-11 12:12:34 +0100

    Graficos
    Parte grafica acabada, falta dar funcionamiento

--- AppCode/VB/app/src/main/java/com/vb/angel/vb/MainActivity.java
+++ AppCode/VB/app/src/main/java/com/vb/angel/vb/MainActivity.java
@@ -7,11 +7,13 @@ import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
-
+    private
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final
     }
 
 
