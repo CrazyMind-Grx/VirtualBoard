4828a07e63a30cb0a74db7d8c6d8619f85be6d0e
Angel JCC <angeljcc@gmail.com>
2014-12-15 20:40:44 +0100

    app

---
+++ AppCode/VirtualBoard-PHP/app/src/main/java/com/vb/iv/virtualboardalpha/Listado.java
@@ -0,0 +1,89 @@
+package com.vb.iv.virtualboardalpha;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.protocol.HttpContext;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+
+import java.util.ArrayList;
+
+public class Listado extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listado);
+        Thread tr = new Thread() {
+            @Override
+            public void run() {
+                final String Resultado = leer();
+                runOnUiThread(
+                        new Runnable() {
+
+                            @Override
+                            public void run() {
+                                cargaListado(obtDatosJSON(Resultado));
+                            }
+                        });
+            }
+        };
+        tr.start();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_listado, menu);
+        return true;
+    }
+
+    public void cargaListado(ArrayList<String> datos) {
+        ArrayAdapter<String> adaptador =
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, datos);
+        ListView listado = (ListView) findViewById(R.id.listView1);
+        listado.setAdapter(adaptador);
+    }
+
+    public String leer() {
+        HttpClient cliente = new DefaultHttpClient();
+        HttpContext contexto = new BasicHttpContext();
+        HttpGet httpget = new HttpGet("http://virtualboardphp-virtualboard.rhcloud.com/GetData.php");
+        String resultado = null;
+        try {
+            HttpResponse response = cliente.execute(httpget, contexto);
+            HttpEntity entity = response.getEntity();
+            resultado = EntityUtils.toString(entity, "UTF-8");
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        return resultado;
+    }
+
+    public ArrayList<String> obtDatosJSON(String response) {
+        ArrayList<String> listado = new ArrayList<String>();
+        try {
+            JSONArray json = new JSONArray(response);
+            String texto = "";
+            for (int i = 0; i < json.length(); i++) {
+                texto = json.getJSONObject(i).getString("nombre") + " - " +
+                        json.getJSONObject(i).getString("texto") + " - " +
+                        json.getJSONObject(i).getString("modo");
+                listado.add(texto);
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        return listado;
+    }
+
+}
