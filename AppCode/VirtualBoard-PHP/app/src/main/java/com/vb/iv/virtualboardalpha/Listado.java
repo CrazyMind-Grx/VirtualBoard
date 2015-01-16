87ae0365f8562e98d28c5ff308d3bb7bb02f0738
javi Aranda <j.arandaizquierdo@gmail.com>
2015-01-16 23:41:09 +0100

    Comentada la clase Listado.java

--- AppCode/VirtualBoard-PHP/app/src/main/java/com/vb/iv/virtualboardalpha/Listado.java
+++ AppCode/VirtualBoard-PHP/app/src/main/java/com/vb/iv/virtualboardalpha/Listado.java
@@ -18,14 +18,24 @@ import org.json.JSONArray;
 
 import java.util.ArrayList;
 
+/**
+ * Esta clase es donde se mostrara la informacion enviada desde el servidor
+ */
 public class Listado extends Activity {
 
+
     @Override
+    /**
+     * Esta funcion leeara de manera remota lo que el servidor envia
+     */
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listado);
+
+        //Creamos la hebra
         Thread tr = new Thread() {
             @Override
+            //Creamos el metodo run
             public void run() {
                 final String Resultado = leer();
                 runOnUiThread(
@@ -41,12 +51,22 @@ public class Listado extends Activity {
         tr.start();
     }
 
+
+    /**
+     * Esta funcion crea el menu de opciones
+     * @param menu
+     * @return
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_listado, menu);
         return true;
     }
 
+    /**
+     * Esta funcion carga los datos leido en un ListView
+     * @param datos los datos que recibimos
+     */
     public void cargaListado(ArrayList<String> datos) {
         ArrayAdapter<String> adaptador =
                 new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, datos);
@@ -54,6 +74,10 @@ public class Listado extends Activity {
         listado.setAdapter(adaptador);
     }
 
+    /**
+     * Esta funcion lee los datos desde el servidor
+     * @return
+     */
     public String leer() {
         HttpClient cliente = new DefaultHttpClient();
         HttpContext contexto = new BasicHttpContext();
@@ -69,6 +93,11 @@ public class Listado extends Activity {
         return resultado;
     }
 
+    /**
+     * Esta funcion obtiene un arraylist desde reponse y se lo asignamos al listado
+     * @param response
+     * @return
+     */
     public ArrayList<String> obtDatosJSON(String response) {
         ArrayList<String> listado = new ArrayList<String>();
         try {
