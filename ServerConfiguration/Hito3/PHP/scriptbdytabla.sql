4a04701380cdec8d74775dad6a0df3816e0fce00
Eduardo José Polo González <willy_edy_@hotmail.com>
2015-01-16 20:05:34 +0100

    Documentacion scriptbdytabla  closes #42

--- ServerConfiguration/Hito3/PHP/scriptbdytabla.sql
+++ ServerConfiguration/Hito3/PHP/scriptbdytabla.sql
@@ -1,6 +1,18 @@
+
+/**
+* Creación de la base de datos virtualboardphp.
+*
+* .
+*/
+
 create database virtualboardphp;
 
 
+/**
+* Creación de la tabla personas en la base de datos virtualboardphp que va contener una identidad
+* que es clave primaria de la tabla, y un nombre, un texto y un modo.
+* .
+*/
 
 CREATE TABLE personas (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
  nombre VARCHAR(20) NOT NULL,
