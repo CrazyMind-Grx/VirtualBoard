e70cf270779e93a6599cf631b0a360a4ff0c6485
Eduardo José Polo González <willy_edy_@hotmail.com>
2015-01-16 19:43:34 +0100

    Creacion de la base de datos y la tabla  closes #41

---
+++ ServerConfiguration/Hito3/PHP/scriptbdytabla.sql
@@ -0,0 +1,11 @@
+create database virtualboardphp;
+
+
+
+CREATE TABLE personas (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+ nombre VARCHAR(20) NOT NULL,
+ texto VARCHAR(500) NOT NULL,
+ modo VARCHAR(20) NOT NULL,
+);
+
+
