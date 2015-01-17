ff341ff90cecffeda872426cc5f098156e844eb8
David Castellón <castellon.david@gmail.com>
2015-01-17 13:43:48 +0100

    Modificando archivos php - Instanciando la nueva clase y llamada a su correspondiente función #43

--- ServerConfiguration/Hito3/PHP/GetData.php
+++ ServerConfiguration/Hito3/PHP/GetData.php
@@ -1,37 +1,7 @@
-
 <?php
 
+$data = new conexionPHPdata();
 
-getdata();
+$data->GetData();
 
-
-
-/**
- *  @brief Funcion para enviar datos a la app y los busca en la base de datos.
- *  
- *  @return string  Devuelve los datos que hay en la base de datos.
- */
- 
-function getdata(){
-
- $con = mysql_connect("127.5.40.130:3306","adminC77ifqE","******") or die("Sin conexion");
-
-  mysql_select_db("virtualboardphp");
-
-  $sql="select id, nombre, texto, modo from personas";
-
-  $datos=array();
-
-  $rs=mysql_query($sql,$con);
-
-  while($row=mysql_fetch_object($rs)){
-
-       $datos[] = $row;
-  }
-
-  echo json_encode($datos);
-}
-  ?>
-
-
-
+?>
--- ServerConfiguration/Hito3/PHP/PutData.php
+++ ServerConfiguration/Hito3/PHP/PutData.php
@@ -1,57 +1,8 @@
 <?php
 
-/**
- *  @author VirtualBoard
- *  @brief Api del Servidor para la conexion de la App
- *  
- *  
- *  
- */
+$data = new conexionPHPdata();
 
-
-
-class PutData{
-
-
-  putdata($_REQUEST["nombre"],$_REQUEST["texto"],$_REQUEST["modo"]);
-
-
-
-
- /**
- *  @brief Funcion para meter datos en la BD
- *  @param string $nombre Primer parametros de entrada en este caso es el nombre de usuario.
- *  @param string $texto Segundo parametro, es el texto que introduce dicho usuario
- *  @param string $modo Es el modo (POST/GET) que el usuario realiza la peticion.
- *  @return string $result devuelve que se ha metido para su verificacion.
- */
- 
-
-  function putdata($nombre,$texto,$modo){
-
-     if ($nombre!="" || $texto!=""){
-
-       $con = mysql_connect("127.5.40.130:3306","adminC77ifqE","*********") or die("Sin conexion");
-
-           mysql_select_db("virtualboardphp");
-
-      echo $sql="insert into personas(nombre, texto, modo) 
-
-               values('$nombre','$texto', '$modo')";
-
-           $result=mysql_query($sql,$con);
-
-           echo $result;
-
-    }else{
-
-       echo "-1";
-
-     }
-  }
-}
-
-
+$data->PutData($_REQUEST["nombre"],$_REQUEST["texto"],$_REQUEST["modo"]);
 
 ?>
 
