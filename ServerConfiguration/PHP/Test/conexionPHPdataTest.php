fb1b7439125f53297d594ab306981db07e40c7a4
Angel JCC <angeljcc@gmail.com>
2015-01-24 12:10:40 +0100

    configuracion de travis

---
+++ ServerConfiguration/PHP/Test/conexionPHPdataTest.php
@@ -0,0 +1,37 @@
+<?php
+
+	require_once('conexionPHPdata.php');
+
+
+	/*
+	 *	Class para testeo
+	 */
+
+	class conexionPHPdataTest extends PHPUnit_Framework_TestCase
+	{
+		/*
+		 *	@brief Testea conexion con BD
+		 */
+		public function test_conexionEstablecida()
+		{
+			
+			$a = new conexionPHPdata();
+		
+			$this->assertTrue($a->GetData() !== false);
+		
+		}
+		/*
+ 		 * @brief Testea la inserccion de elementos en la BD
+ 		 */
+		public function text_insercionCorrecta()
+		{
+		
+			$a = new conexionPHPdata();
+			
+			$this->assertTrue($a->PutData("Test","Texto de prueba","GET") !== false);
+		
+		}
+		
+	}
+
+?>
