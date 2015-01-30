bc7788b179245c731aa2318d8be98991949697f7
David Castellón <castellon.david@gmail.com>
2015-01-30 22:45:20 +0100

    Modificando test para ir reconstruyendo0 #37

--- ServerConfiguration/PHP/test/conexionPHPdataTest.php
+++ ServerConfiguration/PHP/test/conexionPHPdataTest.php
@@ -6,65 +6,10 @@
 
 	class conexionPHPdataTest extends PHPUnit_Framework_TestCase
 	{	
-		private $pdo;
-
-	    public function setUp()
-	    {
-	        $this->pdo = new PDO($GLOBALS['db_dsn'], $GLOBALS['db_username'], $GLOBALS['db_password']);
-	        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	        $this->pdo->query("CREATE TABLE personas (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-													 nombre VARCHAR(20) NOT NULL,
-													 texto VARCHAR(500) NOT NULL,
-													 modo VARCHAR(20) NOT NULL)");
-	    }
-
-    	
-/*
-	    public function testPutData()
-	    {
-	        $data = new src\conexionPHPdata($this->pdo);
-
-	        $this->assertTrue($data->PutData('Nombre','Texto','Modo'));
-	    }
-
-
-	    public function testGetData()
-	    {
-	        $data = new src\conexionPHPdata($this->pdo);
-
-	        $this->assertFalse($data->GetData());
-
-	        $data->PutData('Nombre','Texto','Modo');
-
-
-	        $this->assertTrue($data->GetData());
-	    }
-*/
-		//##################################################################
-		/*
-		 *	@brief Testea conexion con BD
-		 */
-		public function test_conexionEstablecida()
-		{	
-
-			$a = new src\conexionPHPdata();
-		
-			$this->assertTrue($a->GetData() !== false);
-		
-		}
-		/*
- 		 * @brief Testea la inserccion de elementos en la BD
- 		 */
-		public function text_insercionCorrecta()
-		{
-			
-
-			$a = new src\conexionPHPdata();
-			
-			$this->assertTrue($a->PutData("Test","Texto de prueba","GET") !== false);
-		
-		}
-		
+	public function testValidResult(){
+		$calc = new src\prueba;
+		$this->assertEquals(5,$calc->add(2,3));
+	}
 	}
 
 ?>
