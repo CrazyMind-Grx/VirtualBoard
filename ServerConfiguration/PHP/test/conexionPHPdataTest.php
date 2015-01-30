07e26d1550699a2874406747a9fad2198896a0a7
David Castellón <castellon.david@gmail.com>
2015-01-30 22:45:58 +0100

    Actualizando #37

--- ServerConfiguration/PHP/test/conexionPHPdataTest.php
+++ ServerConfiguration/PHP/test/conexionPHPdataTest.php
@@ -7,7 +7,7 @@
 	class conexionPHPdataTest extends PHPUnit_Framework_TestCase
 	{	
 	public function testValidResult(){
-		$calc = new src\prueba;
+		$calc = new src\conexionPHPdata;
 		$this->assertEquals(5,$calc->add(2,3));
 	}
 	}
