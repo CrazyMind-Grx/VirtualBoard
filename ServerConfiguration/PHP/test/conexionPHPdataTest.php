807871ad0d76cb5048ca8c68d161b1378c583df9
David Castellón <castellon.david@gmail.com>
2015-01-31 23:21:47 +0100

    #37 Deshaciendo cambios

--- ServerConfiguration/PHP/test/conexionPHPdataTest.php
+++ ServerConfiguration/PHP/test/conexionPHPdataTest.php
@@ -3,13 +3,11 @@
 	/*
 	 *	Class para testeo
 	 */
-	require_once("../src/conexionPHPdata.php");
 
 	class conexionPHPdataTest extends PHPUnit_Framework_TestCase
 	{	
 		public function testValidResult(){
-			#$calc = new src\conexionPHPdata;
-			$calc = new conexionPHPdata();
+			$calc = new src\conexionPHPdata;
 			$this->assertEquals(5,$calc->add(3,2));
 			$this->assertEquals(true,$calc->PutData("NombreTest", "TextoTest", "GET"));
 			#$this->assertEquals(true,$calc->GetData());
