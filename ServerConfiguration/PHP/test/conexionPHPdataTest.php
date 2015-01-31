fb07e94502ff00d91611351c5d87e3bcc119a2cd
David Castellón <castellon.david@gmail.com>
2015-01-31 23:09:47 +0100

    Enésimo cambio -> require_once #37

--- ServerConfiguration/PHP/test/conexionPHPdataTest.php
+++ ServerConfiguration/PHP/test/conexionPHPdataTest.php
@@ -3,11 +3,13 @@
 	/*
 	 *	Class para testeo
 	 */
+	require_once("../src/conexionPHPdata");
 
 	class conexionPHPdataTest extends PHPUnit_Framework_TestCase
 	{	
 		public function testValidResult(){
-			$calc = new src\conexionPHPdata;
+			#$calc = new src\conexionPHPdata;
+			$calc = new conexionPHPdata();
 			$this->assertEquals(5,$calc->add(3,2));
 			$this->assertEquals(true,$calc->PutData("NombreTest", "TextoTest", "GET"));
 			#$this->assertEquals(true,$calc->GetData());
