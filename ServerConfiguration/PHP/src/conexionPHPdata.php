7a7f5137e6a316c9e540575e30eb2e6ceed5c4b4
David Castellón <castellon.david@gmail.com>
2015-01-31 13:34:49 +0100

    #37 modificando test

--- ServerConfiguration/PHP/src/conexionPHPdata.php
+++ ServerConfiguration/PHP/src/conexionPHPdata.php
@@ -8,10 +8,35 @@ namespace src;
 class conexionPHPdata
 {
 
-public function add($x,$y){
-			return $x + $y;
-		}
-}
+	public function add($x,$y)
+	{
+		return $x + $y;
+	}
 
+	public function PutData($nombre,$texto,$modo)
+	{
+
+		if ($nombre!="" || $texto!=""){
+			
+			$con = mysql_connect("127.5.40.130:3306","adminC77ifqE","Virtualboard1") or die("Sin conexion");
+			mysql_select_db("virtualboardphp");
+
+
+			echo $sql="insert into personas(nombre, texto, modo) values('$nombre','$texto', '$modo')";
+
+			$result=mysql_query($sql,$con);
+
+			echo $result;
+
+			return true;
+
+		}else{
+			echo "-1";
+			return false;
+		}
+
+	}
+
+}
 
 ?>
--- ServerConfiguration/PHP/test/conexionPHPdataTest.php
+++ ServerConfiguration/PHP/test/conexionPHPdataTest.php
@@ -6,10 +6,10 @@
 
 	class conexionPHPdataTest extends PHPUnit_Framework_TestCase
 	{	
-	public function testValidResult(){
-		$calc = new src\conexionPHPdata;
-		$this->assertEquals(5,$calc->add(2,3));
-	} 
+		public function testValidResult(){
+			$calc = new src\conexionPHPdata;
+			$this->assertEquals(true,$calc->PutData("NombreTest", "TextoTest", "GET"));
+		} 
 	}
 
 ?>
