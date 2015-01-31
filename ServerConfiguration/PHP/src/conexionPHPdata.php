ae868d4711df5e284c35368d654d32da600d35b8
David Castellón <castellon.david@gmail.com>
2015-01-31 17:59:56 +0100

    #37 hardcodeando consulta

--- ServerConfiguration/PHP/src/conexionPHPdata.php
+++ ServerConfiguration/PHP/src/conexionPHPdata.php
@@ -15,14 +15,14 @@ class conexionPHPdata
 
 	public function PutData($nombre,$texto,$modo)
 	{
-
+		$result = "1";
 		if ($nombre!="" || $texto!=""){
-			
+			/*
 			$con = mysql_connect("mysql.hostinger.es","u316831537_user","colacao") or die("Sin conexion");
 			mysql_select_db("u316831537_divad");
 			$sql="insert into virtualboard(nombre, texto, modo) values('$nombre','$texto', '$modo')";
 			$result=mysql_query($sql,$con);
-			
+			*/
 			if($result == "1") return true;
 			else return false;
 			
