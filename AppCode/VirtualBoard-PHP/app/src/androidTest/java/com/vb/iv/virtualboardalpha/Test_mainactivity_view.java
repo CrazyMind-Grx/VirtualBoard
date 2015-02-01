caf0aa6124f3dcb5b3315514e04c9a848db1907b
marquina91 <pesadilla91@gmail.com>
2015-02-01 02:15:27 +0100

    mejorando el primer test de la app que funciona #36

--- AppCode/VirtualBoard-PHP/app/src/androidTest/java/com/vb/iv/virtualboardalpha/Test_mainactivity_view.java
+++ AppCode/VirtualBoard-PHP/app/src/androidTest/java/com/vb/iv/virtualboardalpha/Test_mainactivity_view.java
@@ -43,17 +43,26 @@ public class Test_mainactivity_view extends ActivityInstrumentationTestCase2<Mai
 	
 	/**
 	 * Este test comprobará que tenemos en la actividad los label necesarios
-	 * 
+	 * Junto con sus indicaciones para el usuario
 	 */
 	public void test_elementos() {
-		View title = ma.findViewById(R.id.TextView02);
-		assertNotNull(title);
-		
-		TextView text = (TextView)title;
-		
-		//assertNotNull( title.getContentDescription());
+		View label = ma.findViewById(R.id.TextView02);
+		assertNotNull(label);	
+		TextView text = (TextView)label;
 		assertEquals("Nombre", 	text.getText().toString());
-		ViewAsserts.assertOnScreen(origin, title);
+		ViewAsserts.assertOnScreen(origin, label);
+		
+		label = ma.findViewById(R.id.TextView01);
+		assertNotNull(label);	
+		text = (TextView)label;
+		assertEquals("Texto", 	text.getText().toString());
+		ViewAsserts.assertOnScreen(origin, label);
+		
+		label = ma.findViewById(R.id.ck_modo);
+		assertNotNull(label);
+		text = (TextView)label;
+		assertEquals("Enviar por GET", 	text.getText().toString());
+		ViewAsserts.assertOnScreen(origin, label);
 		
 	}
 
