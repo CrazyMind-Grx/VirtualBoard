992a223e6d5d1a4ff7fd3b4c6b300e49021edc2c
Angel Jimenez de Cisneros Carreño <angeljcc@gmail.com>
2015-02-01 17:49:29 +0100

    Ansible completado closes #48sin fallos

--- Aprovisionamiento/crearTabla.sh
+++ Aprovisionamiento/crearTabla.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-#cd ~/VirtualBoard/Aprovisionamiento
-#mysql -h localhost -u virtualboard -p virtualboard
-mysql -u root
-#create database virtualboardphp; #-------modificado
-use virtualboardphp;
-CREATE TABLE personas (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+
+mysql -u root -e "use virtualboardphp; CREATE TABLE personas (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
  nombre VARCHAR(20) NOT NULL,
  texto VARCHAR(500) NOT NULL,
  modo VARCHAR(20) NOT NULL
-);
+);"
