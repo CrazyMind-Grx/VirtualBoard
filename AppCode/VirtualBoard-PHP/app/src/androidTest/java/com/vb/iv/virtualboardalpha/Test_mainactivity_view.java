f81d032d26800f125f0a56019aa5df47bd7e132a
marquina91 <pesadilla91@gmail.com>
2015-02-01 03:05:50 +0100

    primer test de la app que funciona mejorado y documentado #36

--- AppCode/VirtualBoard-PHP/app/src/androidTest/java/com/vb/iv/virtualboardalpha/Test_mainactivity_view.java
+++ AppCode/VirtualBoard-PHP/app/src/androidTest/java/com/vb/iv/virtualboardalpha/Test_mainactivity_view.java
@@ -2,8 +2,6 @@ package com.example.virtualapp.test;
 import com.example.virtualapp.MainActivity;
 import com.example.virtualapp.R;
 
-import android.annotation.TargetApi;
-import android.os.Build;
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.ViewAsserts;
 import android.view.View;
@@ -16,17 +14,26 @@ import android.widget.TextView;
  * @author Marquina
  */
 public class Test_mainactivity_view extends ActivityInstrumentationTestCase2<MainActivity> {
-
+	/**
+	 * Constructor por defecto
+	 */
 	public Test_mainactivity_view() {
 		this("Test para MainActivity elementos");
 	}
 
-
+	/**
+	 * Constructor con string
+	 * @param s String
+	 */
 	public Test_mainactivity_view(String s) {
 		super(MainActivity.class); 
 		setName(s);
 	}
 	
+	/**
+	 * Constructor con string
+	 * @param activityClass actividad a la que realizar test
+	 */
 	public Test_mainactivity_view(Class<MainActivity> activityClass) {
 		super(activityClass);
 	}
@@ -45,25 +52,28 @@ public class Test_mainactivity_view extends ActivityInstrumentationTestCase2<Mai
 	 * Test para comprobar que hemos creado la interfeaz correctamente (todos los elementos)
 	 * Para que el usuario pueda usar la aplicación correctamente
 	 */
-	
-	public void testinicializacion() {
-		View elemento = ma.findViewById(R.id.TextView01);
+	public void test_inicializacion() {
+		View elemento = ma.findViewById(R.id.TextView01);//el campo de texto a escribir
 		assertNotNull(elemento);
 		ViewAsserts.assertOnScreen(origin, elemento);
 		
-		elemento = ma.findViewById(R.id.et_texto);
+		elemento = ma.findViewById(R.id.TextView02);//el campo para introducir login
 		assertNotNull(elemento);
 		ViewAsserts.assertOnScreen(origin, elemento);
 		
-		elemento = ma.findViewById(R.id.ck_modo);
+		elemento = ma.findViewById(R.id.et_texto);//campo de texto 
+		assertNotNull(elemento);
+		ViewAsserts.assertOnScreen(origin, elemento);
+		
+		elemento = ma.findViewById(R.id.ck_modo);//opción de envío
 		assertNotNull(elemento);
 		ViewAsserts.assertOnScreen(origin, elemento);	
 			
-		elemento = ma.findViewById(R.id.button1);		
+		elemento = ma.findViewById(R.id.button1);//boton de envío		
 		assertNotNull(elemento);
 		ViewAsserts.assertOnScreen(origin, elemento);
 		
-		elemento = ma.findViewById(R.id.botonListado);		
+		elemento = ma.findViewById(R.id.botonListado);//boton de listado	
 		assertNotNull(elemento);
 		ViewAsserts.assertOnScreen(origin, elemento);
 
@@ -74,36 +84,36 @@ public class Test_mainactivity_view extends ActivityInstrumentationTestCase2<Mai
 	 * para el usuario.
 	 */
 	public void test_elementos() {
-		View label = ma.findViewById(R.id.TextView02);
+		View label = ma.findViewById(R.id.TextView02);//el campo para introducir login
 		assertNotNull(label);	
 		TextView text = (TextView)label;
 		assertEquals("Nombre", 	text.getText().toString());
 		ViewAsserts.assertOnScreen(origin, label);
 		
-		label = ma.findViewById(R.id.TextView01);
+		label = ma.findViewById(R.id.TextView01);//campo de texto a escribir
 		assertNotNull(label);	
 		text = (TextView)label;
 		assertEquals("Texto", text.getText().toString());
 		ViewAsserts.assertOnScreen(origin, label);
 		
-		label = ma.findViewById(R.id.ck_modo);
+		label = ma.findViewById(R.id.ck_modo);//opción para enviar mediante GET
 		assertNotNull(label);
 		text = (TextView)label;
 		assertEquals("Enviar por GET", 	text.getText().toString());
 		ViewAsserts.assertOnScreen(origin, label);
 		
-		label = ma.findViewById(R.id.button1);
+		label = ma.findViewById(R.id.button1);//boton de envío
 		assertNotNull(label);
 		text = (TextView)label;
 		assertEquals("Enviar", 	text.getText().toString());
 		ViewAsserts.assertOnScreen(origin, label);
 		
-		label = ma.findViewById(R.id.botonListado);
+		label = ma.findViewById(R.id.botonListado);//boton de listado
 		assertNotNull(label);
 		text = (TextView)label;
 		assertEquals("Listado", text.getText().toString());
 		ViewAsserts.assertOnScreen(origin, label);
 		
 	}
-
+	
 }
