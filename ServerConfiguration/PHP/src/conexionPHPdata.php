e8d6fe54963b966e36efef2ecfbb3fa224782ed7
Angel Jimenez de Cisneros Carreño <angeljcc@gmail.com>
2015-02-01 11:59:41 +0100

    updates para IaaS Azure #43

--- ServerConfiguration/PHP/src/conexionPHPdata.php
+++ ServerConfiguration/PHP/src/conexionPHPdata.php
@@ -18,7 +18,7 @@ class conexionPHPdata
 		if ($nombre!="" || $texto!=""){
 			
 
-			$con=mysqli_connect("mysql.hostinger.es","u316831537_user","colacao","u316831537_divad");
+			$con=mysqli_connect("127.0.0.1","virtualboard","virtualboard","virtualboardphp");
 			$q="insert into virtualboard(nombre, texto, modo) values('$nombre','$texto', '$modo')";
 			if(mysqli_query($con, $q))
 			{
@@ -40,8 +40,8 @@ class conexionPHPdata
 		
  		$conexion = false;
  	
-		$con = mysqli_connect("mysql.hostinger.es","u316831537_user","colacao") or die("Sin conexion");
-		mysql_select_db("u316831537_divad");
+		$con = mysqli_connect("127.0.0.1","virtualboard","virtualboard","virtualboardphp") or die("Sin conexion");
+		//mysql_select_db("u316831537_divad");
 		$sql="select id, nombre, texto, modo from personas";
 
 		$datos=array();
