2ed93f2658b222bbc6f0bb6547559202ca3bd1c7
marquina91 <pesadilla91@gmail.com>
2015-02-02 15:49:25 +0100

    metadata

--- Aprovisionamiento/aprovisionamiento_chef/recetaschef.sh
+++ Aprovisionamiento/aprovisionamiento_chef/recetaschef.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 mkdir -p chef/cookbooks/apache2/recipes
 mkdir -p chef/cookbooks/mysql/recipes
 mkdir -p chef/cookbooks/php/recipes
@@ -23,13 +22,37 @@ echo "{
  ] 
 }" > chef/node.json
 
-echo file_cache_path \"/chef\" > chef/solo.rb
-echo cookbook_path \"/chef/cookbooks\" >> chef/solo.rb
-echo json_attribs \"/chef/node.json\" >> chef/solo.rb
+echo "maintainer       \"VirtualBoard\"
+maintainer_email  \"vitualboard@gmail.com\"
+description       \"Instala servidor Apache \"
+version           \"0.1 \"
 
-sudo apt-get install chef
+recipe  \"apache2 \",  \"Despliegue servidor web \" ">  chef/cookbooks/apache2/metadato.rb
+
+
+echo "maintainer       \"VirtualBoard\"
+maintainer_email  \"vitualboard@gmail.com\"
+description       \"Instala mysql \"
+version           \"0.1 \"
+
+recipe  \"mysql \",  \"Despliegue de mysql \" ">  chef/cookbooks/apache2/metadato.rb
+
+
+echo "maintainer       \"VirtualBoard\"
+maintainer_email  \"vitualboard@gmail.com\"
+description       \"Instala php \"
+version           \"0.1 \"
+
+recipe  \"php\",  \"Despliegue de php\" ">  chef/cookbooks/apache2/metadato.rb
+
+
+echo file_cache_path \"/home/marquina/Desktop/chef\" > chef/solo.rb
+echo cookbook_path \"/home/marquina/Desktop/chef/cookbooks\" >> chef/solo.rb
+echo json_attribs \"/home/marquina/Desktop/chef/node.json\" >> chef/solo.rb
 
 git clone  https://github.com/IV-2014/VirtualBoard.git
 
-sudo chef-solo -c /chef/solo.rb 
+sudo apt-get install chef
+
+sudo chef-solo -c /home/marquina/Desktop/chef/solo.rb 
 
