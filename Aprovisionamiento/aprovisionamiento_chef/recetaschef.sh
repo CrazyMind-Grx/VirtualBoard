db073f9dfb42a156a9cf559f9c243d8a1d9953a4
Eduardo J. Polo Gonzalez <willy_edy_@hotmail.com>
2015-02-02 12:52:44 +0100

    creo los default.rb #53

--- Aprovisionamiento/aprovisionamiento_chef/recetaschef.sh
+++ Aprovisionamiento/aprovisionamiento_chef/recetaschef.sh
@@ -5,3 +5,7 @@ mkdir -p chef/cookbooks/mysql/recipes
 mkdir -p chef/cookbooks/php/recipes
 mkdir -p chef/cookbooks/git/recipes
 
+touch chef/cookbooks/apache2/recipes/default.rb
+touch chef/cookbooks/mysql/recipes/default.rb
+touch chef/cookbooks/php/recipes/default.rb
+touch chef/cookbooks/git/recipes/default.rb
