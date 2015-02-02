24311b4c25397b8fbc0881ca279e50537b8507cd
Juan Marquina <pesadilla91@gmail.com>
2015-02-02 13:09:22 +0100

    update

--- Aprovisionamiento/aprovisionamiento_chef/recetaschef.sh
+++ Aprovisionamiento/aprovisionamiento_chef/recetaschef.sh
@@ -7,5 +7,6 @@ mkdir -p chef/cookbooks/git/recipes
 
 echo package \'apache2\' >  chef/cookbooks/apache2/recipes/default.rb
 echo package \'mysql\' >  chef/cookbooks/mysql/recipes/default.rb
-
+echo package \'php\' >  chef/cookbooks/php/recipes/default.rb
+echo package \'git\' >  chef/cookbooks/git/recipes/default.rb
 
