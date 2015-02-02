85e624f16412d1ed624738b922af6990e07df2f9
Juan Marquina <pesadilla91@gmail.com>
2015-02-02 13:37:29 +0100

    añadidos paquetes mysql #53

--- Aprovisionamiento/aprovisionamiento_chef/recetaschef.sh
+++ Aprovisionamiento/aprovisionamiento_chef/recetaschef.sh
@@ -5,8 +5,9 @@ mkdir -p chef/cookbooks/mysql/recipes
 mkdir -p chef/cookbooks/php/recipes
 mkdir -p chef/cookbooks/git/recipes
 
-echo package \'apache2\' >  chef/cookbooks/apache2/recipes/default.rb
-echo package \'mysql\' >  chef/cookbooks/mysql/recipes/default.rb
-echo package \'php\' >  chef/cookbooks/php/recipes/default.rb
-echo package \'git\' >  chef/cookbooks/git/recipes/default.rb
-
+echo package \'apache2\' > chef/cookbooks/apache2/recipes/default.rb
+echo package \'python-mysqldb\' >> chef/cookbooks/mysql/recipes/default.rb
+echo package \'mysql-server\' >> chef/cookbooks/mysql/recipes/default.rb
+echo package \'mysql-client\' >> chef/cookbooks/mysql/recipes/default.rb
+echo package \'php\' > chef/cookbooks/php/recipes/default.rb
+echo package \'git\' > chef/cookbooks/git/recipes/default.rb
