7aef1a892197fe2a73d075a83655432c08af5246
Eduardo J. Polo Gonzalez <willy_edy_@hotmail.com>
2015-02-02 16:04:53 +0100

    automatización de la creación de la tabla #53

--- Aprovisionamiento/aprovisionamiento_chef/recetaschef.sh
+++ Aprovisionamiento/aprovisionamiento_chef/recetaschef.sh
@@ -63,3 +63,8 @@ sudo apt-get install chef
 
 sudo chef-solo -c /chef/solo.rb 
 
+mysql -u root -e " CREATE DATABASE virtualboardphp;use virtualboardphp; CREATE TABLE personas (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+nombre VARCHAR(20) NOT NULL,
+texto VARCHAR(500) NOT NULL,
+modo VARCHAR(20) NOT NULL
+);"
