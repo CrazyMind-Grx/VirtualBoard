a24b02d109d9c525f65074848175b414ca50b738
Javier Aranda Izquierdo <j.arandaizquierdo@gmail.com>
2015-03-25 00:49:01 +0100

    Creada la clase Vista

--- AppCode/Dibujando/app/src/main/java/com/example/javi/dibujando/MainActivity.java
+++ AppCode/Dibujando/app/src/main/java/com/example/javi/dibujando/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.javi.dibujando;
 
+import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -11,9 +13,15 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView();
     }
+    //Creamos nuestra propia vista
+    class Pizarra extends View{
 
+        public Pizarra(Context context) {
+            super(context);
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
