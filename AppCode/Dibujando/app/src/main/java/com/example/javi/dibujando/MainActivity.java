4ede8b81848a369adfedb53f83fe2be8abd1a163
Javier Aranda Izquierdo <j.arandaizquierdo@gmail.com>
2015-03-25 01:31:53 +0100

    Primera version para pintar terminada #55

--- AppCode/Dibujando/app/src/main/java/com/example/javi/dibujando/MainActivity.java
+++ AppCode/Dibujando/app/src/main/java/com/example/javi/dibujando/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.javi.dibujando;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 
 
@@ -13,13 +18,58 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView();
+        Pizarra pizarra=new Pizarra(this);
+        setContentView(pizarra);
     }
     //Creamos nuestra propia vista
     class Pizarra extends View{
 
+        float x = 0;
+        float y = 0;
+        String accion= "accion";
+        
+        Path path = new Path(); // Encapsula varios tipos de caminos geometricos
+
         public Pizarra(Context context) {
             super(context);
+        }
+        /*
+        onDrew es el metodo que se utiliza para dibujar
+         */
+        public void onDraw (Canvas canvas){
+            Paint paint = new Paint();
+            paint.setStyle(Paint.Style.STROKE);//Dibujamos un tipo de linea stroke
+            paint.setStrokeWidth(5);
+            paint.setColor(Color.BLACK);
+
+            if(accion== "down"){
+                path.moveTo(x,y);//Desde este punto comienza nuestra linea a dibujar
+            }
+
+            if (accion=="move"){
+                path.lineTo(x,y);//Sigue el camino de la linea
+            }
+
+            canvas.drawPath(path,paint);
+        }
+        
+        /*Gestiona los eventos al tocar la pantalla*/
+        public boolean onTouchEvent(MotionEvent e){
+
+            x=e.getX();//Nos devuelve el valor x de la posicion donde tocamos
+            y=e.getY();//Nos devuelve el valor y de la posicion donde tocamos
+
+            if(e.getAction()==MotionEvent.ACTION_DOWN)//Al pulsar la prantalla...
+            {
+                accion="down";
+            }
+            if(e.getAction()==MotionEvent.ACTION_MOVE)//Al mover el dedo sobre la pantalla....
+            {
+                accion="move";
+            }
+
+        invalidate();
+        return true;
         }
     }
 
